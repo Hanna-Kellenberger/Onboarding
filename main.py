8c610b24bc9ca89521710fb1c4cd011ff2f279c8
Hanna-Kellenberger <hannarosekell@gmail.com>
2026-02-17 00:00:43 -0600

    main bulk of program.
    Looked through the data and got rid of any missing values. Visualized what I found.

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+df = pd.read_csv('can_data.csv')
+
+# clean the data
+df.dropna(inplace=True)
+df["timestamp"] = pd.to_datetime(df["timestamp"])
+
+# visualize the data
+plt.figure()
+plt.title("TPS and wheel speed over Time")
+plt.plot(df["timestamp"], df["TPS"])
+plt.plot(df["timestamp"], df["Driven Wheel Speed #1"])
+plt.xlabel("Timestamp")
+plt.ylabel("TPS and Driven Wheel Speed #1")
+plt.legend(["TPS", "Driven Wheel Speed #1"])
+
+plt.figure()
+plt.title("RPM vs TPS")
+plt.scatter(df["RPM"], df["TPS"])
+plt.xlabel("RPM")
+plt.ylabel("TPS") 
+
+plt.figure()
+plt.title("RPM vs wheel speed")
+plt.plot(df["RPM"], df["Driven Wheel Speed #1"],'o')
+plt.xlabel("RPM")
+plt.ylabel("Driven Wheel Speed #1") 
+
+plt.show()
